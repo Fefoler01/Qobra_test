c10a93deb8198eb4b3b06aef87e494cb9c91beb2
Fefoler01 <72406859+Fefoler01@users.noreply.github.com>
2022-10-05 22:47:21 +0200

    clean and comment level 1+2

--- serpentini-candidate/level1/lvl1.py
+++ serpentini-candidate/level1/lvl1.py
@@ -1,4 +1,3 @@
-# 
 
 # import libraries
 import json
@@ -6,22 +5,20 @@ import json
 # read in data
 with open('./data/input.json') as f:
     jsonData = json.load(f)
-#print(jsonData)
 
-# parse data
+# parse data to get the total for each user
 user = {}
 for deal in jsonData["deals"]:
-    #print(deal)
-    if deal["user"] not in user:
+    if deal["user"] not in user: # initialize data in the user dict (only once to create the dict)
         user[deal["user"]] = {}
         user[deal["user"]]["total"] = deal["amount"]
         user[deal["user"]]["count"] = 1
-    else:
+    else: # update data in the user dict
         user[deal["user"]]["total"] += deal["amount"]
         user[deal["user"]]["count"] += 1
 #print(user)
 
-# transform data
+# calculate commissions for each user and write to output file
 userDict = {"commissions": []}
 for id in user:
     bonus=0
@@ -34,7 +31,7 @@ for id in user:
         userDict["commissions"].append({"user_id": id, "commission": str(round(user[id]["total"]*0.2,2)+bonus)})
     else:
         userDict["commissions"].append({"user_id": id, "commission": str(round(user[id]["total"]*0.1,2)+bonus)})
-#print(userDict)
+print(userDict)
 
 # write out data
 with open('./data/output.json', 'w') as f:
--- serpentini-candidate/level2/lvl2.js
+++ serpentini-candidate/level2/lvl2.js
@@ -1,51 +1,42 @@
 
-
 // import json file
 import jsonData from './data/input.json' assert { type: 'json' };
 
-console.log(jsonData);
-
 // sum of deals by user
 var user = {};
-for (var i_deal = 0; i_deal < jsonData.deals.length; i_deal++) {
-    if (jsonData.deals[i_deal].user in user) {
+for (var i_deal = 0; i_deal < jsonData.deals.length; i_deal++) { // loop through deals
+    if (jsonData.deals[i_deal].user in user) { // if user already exists
         user[jsonData.deals[i_deal].user] += jsonData.deals[i_deal].amount;
-    } else {
+    } else { // first deal for this user
         user[jsonData.deals[i_deal].user] = jsonData.deals[i_deal].amount;
     }
 }
 console.log(user);
 
-// comparaison between deals and objectives
+// comparaison between deals and objectives to calculate commissions
 var result = {"commissions":[]};
 for (var i_user in user) {
-    console.log(user[i_user]);
-    var objective = 100*user[i_user] / jsonData.users[i_user-1].objective;
+    var objective = 100*user[i_user] / jsonData.users[i_user-1].objective; // objective reach by user in %
 
     result.commissions[i_user-1] = {};
     result.commissions[i_user-1].user_id = i_user;
     result.commissions[i_user-1].commission = 0;
-    if (objective > 100) {
-        result.commissions[i_user-1].commission += (user[i_user]-jsonData.users[i_user-1].objective)*0.15;
+    if (objective > 100) { // for commission part over 100%
+        result.commissions[i_user-1].commission += (user[i_user]-jsonData.users[i_user-1].objective)*0.15; // total - objective
     }
-    if (objective > 50) {
-        result.commissions[i_user-1].commission += (user[i_user]-result.commissions[i_user-1].commission-jsonData.users[i_user-1].objective*0.5)*0.1;
+    if (objective > 50) { // for commission part over 50%
+        result.commissions[i_user-1].commission += (user[i_user]-result.commissions[i_user-1].commission-jsonData.users[i_user-1].objective*0.5)*0.1; // total - commission>100% - demi-objective
     }
-    result.commissions[i_user-1].commission += (user[i_user]-result.commissions[i_user-1].commission)*0.05;
+    // for commission part under 50%
+    result.commissions[i_user-1].commission += (user[i_user]-result.commissions[i_user-1].commission)*0.05; // total - (commission>100% + commission>50%)
         
 }
 console.log(result);
 
-/* write result in json file */
 
-
-
-/* write result in json file
+// write result in json file
 const fs = require('fs');
 fs.writeFile('./data/output.json', JSON.stringify(result), (err) => {
     if (err) throw err;
     console.log('Complete!');
-});*/
-//export default JSON.stringify(result);
-
-console.log('End');
+});
